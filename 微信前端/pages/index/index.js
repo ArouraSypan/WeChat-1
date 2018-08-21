341d8bf51b3db8dba4398f150ed82322d6dea680
Sehunwy <625192678@qq.com>
2018-08-21 14:49:20 +0800

    更新代码

--- 微信前端/pages/index/index.js
+++ 微信前端/pages/index/index.js
@@ -4,7 +4,18 @@ const app = getApp()
 
 Page({
   data: {
-    isChecked: false
+    isChecked: false,
+      imgUrls: [
+        '../../imgs/m1.jpg',
+        '../../imgs/m2.jpg',
+        '../../imgs/m3.jpg',
+        '../../imgs/m4.jpg',
+        '../../imgs/m5.jpg',
+        '../../imgs/m6.jpg'
+      ],
+      autoplay: true,
+      interval: 2000,
+      duration: 1000
   },
   serviceSelection: function (e) {
     var judge = true;
